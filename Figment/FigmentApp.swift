179431315068fbe3abb5c400316fe710eea0af3c
Sacha Sedriks <sachased@gmail.com>
2025-12-02 21:01:41 -0800

    Initial Commit

---
+++ Figment/FigmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmentApp.swift
+//  Figment
+//
+//  Created by Sacha Sedriks on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FigmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
